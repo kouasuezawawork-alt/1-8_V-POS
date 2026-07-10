2d386c0037bdb223cf12d3fb277556ae5d91545c
KOUA <koua.suezawa.work@gmail.com>
2026-07-10 22:08:21 +0900

    8

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./styles/main.css";
 import "./styles/magic.css";
 import "./styles/parchment.css";
 import "./styles/animation.css";
+import "./styles/register.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
-import "./styles/register.css";
